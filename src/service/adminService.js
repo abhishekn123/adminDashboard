f152278a0cd9f3810806a5597fbe185235380ad4
abhishekn123 <abhiabhishekn164@gmail.com>
2021-06-27 21:45:48 +0530

    servicedata changed

--- src/service/adminService.js
+++ src/service/adminService.js
@@ -5,15 +5,5 @@ export function getRooms() {
       name: "Room 1",
       units: [{ id: 0, name: "", components: []}],
     },
-    {
-      id: 1,
-      name: "Room 2",
-      units: [{ id: 1, name: "", components: []}],
-    },
-    {
-      id: 2,
-      name: "Room 3",
-      units: [{ id: 1, name: "", components: [] }],
-    },
   ];
 }
